368355aff804b5eab4e04b7471e940140d9f4402
Mikhail-Zoreev <43145054+BFDestroyeer@users.noreply.github.com>
2021-03-11 14:34:31 +0300

    fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
+#include "sequentialdirichlet.h"
+#include <iostream>
+
 int main(int argc, char *argv[]) {
+    auto result =
+        sequentialDirichlet([](double x, double y) { return x * y; }, [](double x, double y) { return x + y; }, 5, 1.0);
+    for (size_t i = 0; i < 5; i++) {
+        for (size_t j = 0; j < 5; j++) {
+            std::cout << result(i, j) << ' ';
+        }
+        std::cout << "\n";
+    }
     return 0;
 }
--- src/sequentialdirichlet.cpp
+++ src/sequentialdirichlet.cpp
@@ -7,14 +7,14 @@ Network sequentialDirichlet(std::function<double(double, double)> f, std::functi
     }
     Network u(node_count), f_network(node_count);
     // Initialization
-    for (size_t i = 0; i < node_count - 1; i++) {
+    for (size_t i = 0; i < node_count; i++) {
         double shift = i * (1 / static_cast<double>(node_count - 1));
         u(0, i) = g(0, shift);
         u(i, 0) = g(shift, 0);
         u(i, node_count - 1) = g(shift, 1);
         u(node_count - 1, i) = g(1, shift);
     }
-    for (size_t i = 0; i < node_count - 1; i++) {
+    for (size_t i = 0; i < node_count; i++) {
         for (size_t j = 0; j < node_count; j++) {
             f_network(i, j) =
                 f(i * (1 / static_cast<double>(node_count - 1)), j * (1 / static_cast<double>(node_count - 1)));
@@ -25,13 +25,13 @@ Network sequentialDirichlet(std::function<double(double, double)> f, std::functi
     double delta, max_delta, h = 1 / static_cast<double>(node_count - 1);
     do {
         max_delta = 0;
-        for (size_t i = 1; i < node_count; i++) {
-            for (size_t j = 1; j < node_count; j++) {
-                double temp;
-                u(i, j) = 0.25 * (u(i - 1, j) + u(i + 1, j) + u(i, j - 1) + u(i, j + 1) - h * h * f(i, j));
+        for (size_t i = 1; i < node_count - 1; i++) {
+            for (size_t j = 1; j < node_count - 1; j++) {
+                double temp = u(i, j);
+                u(i, j) = 0.25 * (u(i - 1, j) + u(i + 1, j) + u(i, j - 1) + u(i, j + 1) - h * h * f_network(i, j));
                 delta = std::fabs(temp - u(i, j));
                 if (max_delta < delta) {
-                    max_delta == delta;
+                    max_delta = delta;
                 }
             }
         }
